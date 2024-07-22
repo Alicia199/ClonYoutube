63c683d93b3a342ba69e7a3883b7de91b33d76f9
Alicia199 <alibermudz97@gmail.com>
2024-07-22 21:55:19 +0200

    new

--- codigo.js
+++ codigo.js
@@ -14,3 +14,17 @@ document.getElementById('toggleButton').addEventListener('click', function() {
     let menuLateral = document.getElementById('menu__lateral');
     menuLateral.classList.toggle('minimized');
 });
+
+document.getElementById('prev').addEventListener('click', function() {
+    document.querySelector('.categorias-wrapper').scrollBy({
+        left: -100,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('next').addEventListener('click', function() {
+    document.querySelector('.categorias-wrapper').scrollBy({
+        left: 100,
+        behavior: 'smooth'
+    });
+});
